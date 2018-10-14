5df6f2f4adb62c1ce31ac3117702489e5028cf95
raklane <rakesh.xyzz@gmail.com>
2018-10-14 01:23:41 -0500

    14th oct

--- Miwok/app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -3,7 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,13 +24,10 @@ public class NumberActivity extends AppCompatActivity {
         ArrayList<String> words = new ArrayList<String>();
         words.addAll(Arrays.asList(wordsArray));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
-        for(int index = 0; index<words.size() ;index++){
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
     }
 }
